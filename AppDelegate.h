5be3022648516a8be9662a401472977693e4aa51
NSLegend <1274478840@qq.com>
2017-08-13 19:01:08 +0800

    Initial commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by xiaowu on 2017/8/13.
+//  Copyright © 2017年 xiaowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
